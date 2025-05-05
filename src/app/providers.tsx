abdbbc7187acd04ba9989811986360dbe0e146dc
MhmdNdri <m.n.mohammad.naderi@gmail.com>
2025-05-05 14:17:25 +0100

    add PostHog for analytics tracking

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,21 +4,24 @@ import { ThemeProvider } from "next-themes";
 import { ClerkProvider } from "@clerk/nextjs";
 import { QueryProvider } from "@/components/providers/query-provider";
 import { ToastProvider } from "@/components/providers/toast-provider";
+import { PostHogProvider } from "@/components/providers/posthog-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <QueryProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <ToastProvider />
-          {children}
-        </ThemeProvider>
-      </QueryProvider>
-    </ClerkProvider>
+    <PostHogProvider>
+      <ClerkProvider>
+        <QueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ToastProvider />
+            {children}
+          </ThemeProvider>
+        </QueryProvider>
+      </ClerkProvider>
+    </PostHogProvider>
   );
 }
