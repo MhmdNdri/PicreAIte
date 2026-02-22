137b10e72f9d2cf58f322b6bd422cfca0e6ee48a
MhmdNdri <m.n.mohammad.naderi@gmail.com>
2026-02-22 20:25:07 +0000

    refactor: restructure Providers component to simplify provider hierarchy

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,36 +2,26 @@
 
 import { ThemeProvider } from "next-themes";
 import { ClerkProvider } from "@clerk/nextjs";
-import { usePathname } from "next/navigation";
 import { QueryProvider } from "@/components/providers/query-provider";
 import { ToastProvider } from "@/components/providers/toast-provider";
 import { PostHogProvider } from "@/components/providers/posthog-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const isHomePage = pathname === "/";
-
-  const baseProviders = (
-    <QueryProvider>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <ToastProvider />
-        {children}
-      </ThemeProvider>
-    </QueryProvider>
-  );
-
-  if (isHomePage) {
-    return baseProviders;
-  }
-
   return (
     <PostHogProvider>
-      <ClerkProvider>{baseProviders}</ClerkProvider>
+      <ClerkProvider>
+        <QueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ToastProvider />
+            {children}
+          </ThemeProvider>
+        </QueryProvider>
+      </ClerkProvider>
     </PostHogProvider>
   );
 }
